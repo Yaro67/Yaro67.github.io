66e708caa00306b496c245aa77a4d30ca94f141d
Yaro67 <139159432+Yaro67@users.noreply.github.com>
2023-11-10 13:48:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def mainroute():
-  return render_template('index.html')
+
+  testvar = 'bye'
+
+  if request.method == 'GET':
+    pass
+
+  if request.method == 'POST':
+    testvar = 'hi'
+  
+  return render_template('index.html', testvar = testvar)
 
 app.run(host = '0.0.0.0')
